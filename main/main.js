34591c2905799436fdbbc7ea69d947a2e1390a0f
hiltar <tartsihiltsu@gmail.com>
2020-11-24 22:42:56 +0200

    Laitettu view-engine, render index ja palvelimen avaus

--- main/main.js
+++ main/main.js
@@ -1,1 +1,14 @@
+const express = require('express');
+const app = express();
 
+//view engine
+app.set('view engine', 'ejs');
+
+//renderz
+app.get('/', (req, res) => {
+  res.render('index')
+});
+
+//localhost:3000
+app.listen(3000);
+console.log('Blogi avattu osoitteeseen localhost:3000');
