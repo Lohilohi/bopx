2bd59f0e3b35f41f7d7b301146ed7b9c08a7511f
Lohilohi <44428565+Lohilohi@users.noreply.github.com>
2020-12-03 11:25:03 +0200

    Lisättiin viimeiset tarvittavat kansiot ja tiedostot
    Lisäsin viimeiset tarvittavat kansiot ja tiedostot sekä fontteja, kuvia ja CSS:ssää

--- main/main.js
+++ main/main.js
@@ -1,14 +1,33 @@
-const express = require('express');
-const app = express();
+const express = require('express')
+const mongoose = require('mongoose')
+const blogArticle = require('./models/blog_models')
+const aboutBlogger = require('./models/about_models')
+const blogArticleRouter = require('./routes/blog_routes')
+const aboutRouter = require('./routes/about_routes')
+const methodOverride = require('method-override')
+const app = express()
 
-//view engine
-app.set('view engine', 'ejs');
+mongoose.connect('mongodb://localhost/blog', {
+  useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true
+})
 
-//renderz
-app.get('/', (req, res) => {
-  res.render('index')
-});
+app.set('view engine', 'ejs')
+app.use(express.urlencoded({ extended: false }))
+app.use(methodOverride('_method'))
 
-//localhost:3000
-app.listen(3000);
-console.log('Blogi avattu osoitteeseen localhost:3000');
+app.get('/', async (req, res) => {
+  const articles = await blogArticle.find().sort({ published: 'desc' })
+  const total_post = articles.length
+  return res.render('blog_views/index', { articles: articles, total_post: total_post })
+})
+
+app.get('/about', async (req, res) => {
+  const about_author = await aboutBlogger.find().sort({ date_time_track: 'desc' })
+  return res.render('about_views/about', { about_author: about_author })
+})
+// for linking css/other static assets
+app.use(express.static(__dirname + '/public'))
+app.use('/', blogArticleRouter)
+app.use('/about', aboutRouter)
+
+app.listen(5000)
